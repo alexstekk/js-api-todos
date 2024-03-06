10bb75255f45af613380741c8854d541be3bc03b
Alexander Stekolshchikov <alexstekk@yandex.ru>
2024-03-06 10:54:34 +0300

    Get data from server

---
+++ app.js
@@ -0,0 +1,28 @@
+// Global variables
+let todos = [];
+let users = [];
+
+// Attach Events
+document.addEventListener('DOMContentLoaded', initApp);
+
+// Event logic
+function initApp() {
+	Promise.all([getAllTodos(), getAllUsers()]).then((values) => {
+		[todos, users] = values;
+	});
+}
+
+// Async logic
+async function getAllTodos() {
+	const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+	const data = await response.json();
+
+	return data;
+}
+
+async function getAllUsers() {
+	const response = await fetch('https://jsonplaceholder.typicode.com/users');
+	const data = await response.json();
+
+	return data;
+}
